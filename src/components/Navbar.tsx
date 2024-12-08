6ff0e61cc63b91ed689d6809f686339f07e4d070
Subhram Subhrajyoti Rana <Subhram@MacBookPro.local>
2024-12-08 16:37:11 +0530

    practive code

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+function Navbar() {
+    return (
+        // <nav className="navbar navbar-expand-lg" style={{
+        //     backgroundColor: '#4D545B',
+        // }}>
+        //     <div className="container-fluid">
+        //         <a className="navbar-brand" href="#">
+        //             {/*Subhram*/}
+        //             <img src='/public/white-logo-removebg.ico'/>
+        //         </a>
+        //     </div>
+        // </nav>
+        <button type="button" className="btn btn-secondary">Secondary</button>
+    );
+}
+
+export default Navbar;
